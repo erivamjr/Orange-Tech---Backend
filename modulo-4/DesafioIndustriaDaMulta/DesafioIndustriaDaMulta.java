30550de5134488e95201a1fd4d38d767a9aca010
José Erivam <erivamdev@gmail.com>
2023-01-27 20:27:05 -0300

    :sparkles: feat:add challenge 5

--- modulo-4/DesafioIndustriaDaMulta/DesafioIndustriaDaMulta.java
+++ modulo-4/DesafioIndustriaDaMulta/DesafioIndustriaDaMulta.java
@@ -16,21 +16,3 @@ public class DesafioIndustriaDaMulta {
 
     }
 }
-/**
- * Desafio
- * A prefeitura de uma cidade A instalou uma câmera para verificar quem estava acima da velocidade permitida na rodovia da entrada da cidade. Porém, ela é uma cidade extremamente movimentada e a prefeitura não deu conta de enviar as multas necessárias para quem ultrapassasse a velocidade permitida de 60km/h.
- * <p>
- * O seu papel será o de criar um programa que verifique os dados recebidos da câmera e envie uma mensagem dizendo se o motorista será multado ou não.
- * <p>
- * Entrada
- * A entrada vai ser a velocidade do motorista.
- * <p>
- * Saída
- * A saída será uma mensagem no console dizendo se o motorista foi multado ou não. (sem as aspas)
- * <p>
- * Exemplo
- * Entrada	Saída
- * 60	 "Nao foi multado"
- * 68	    "Foi multado"
- * 100	    "Foi multado"
- */
