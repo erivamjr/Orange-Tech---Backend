c95765f130a1e1be3541bc53ffeccfd7cca38693
José Erivam <erivamdev@gmail.com>
2023-01-27 20:14:44 -0300

    :sparkles: feat:add challenge 3

--- modulo-4/DesafioLeituraDaGertrudes/DesafioLeituraDaGertrudes.java
+++ modulo-4/DesafioLeituraDaGertrudes/DesafioLeituraDaGertrudes.java
@@ -15,19 +15,4 @@ public class DesafioLeituraDaGertrudes {
         }
     }
 }
-/**
- * Desafio
- * Sua tia Gertrudes é uma leitora assídua. E ela gostaria de ter um aplicativo que dissesse para ela, de acordo com o número de páginas de um livro, quanto tempo ela levaria para ler lendo apenas 3 páginas por dia. Como você está aprendendo Java, você se disponibilizou para ajudá-la com esse desenvolvimento.
- * <p>
- * Entrada
- * A entrada será o número de páginas de um determinado livro
- * <p>
- * Saída
- * A saída deverá ser quanto tempo ela vai levar para ler esse livro
- * <p>
- * Exemplo
- * Entrada	Saída
- * 30	    "10 dias"
- * 15	      "5 dias"
- * 90	    "30 dias"
- */
+
